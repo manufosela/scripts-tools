ba670029c7f82449795b7b5dc600d3c3decb5925
Manuel Juan Fosela Águila <manufosela@gmail.com>
2022-04-14 18:49:09 +0200

    first commit

---
+++ config-npm.sh
@@ -0,0 +1,5 @@
+npm config set init-author-name "manufosela"
+npm config set init-author-email "mjfosela@gmail.com"
+npm config set init-author-url "https://manufosela.es"
+npm config set init-license "MIT"
+npm config set init-version "1.0.0"
